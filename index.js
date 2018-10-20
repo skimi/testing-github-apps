5c9c07e5040751536c4c87b7d18365e1662d2c88
Remi Gebski <rgebski@lafourchette.com>
2018-10-20 18:56:59 +0200

    log all requests

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const PORT = process.env.PORT || 5000
+const inspect = require('util').inspect
 
 express()
-  .get('/', (req, res) => res.send('pouet'))
+  .get('/', (req, res) => {
+    process.stdout.write(inspect(req));
+    res.send('pouet')
+  })
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
